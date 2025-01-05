a040c1475d8d616ede4cc58c0ed80a226a32d348
Hubby <hubby@rmhubbert.com>
2025-01-05 17:08:31 +0000

    test: add unit tests for composite function

--- composite_test.go
+++ composite_test.go
@@ -51,8 +51,8 @@ func Test_line(t *testing.T) {
 	}
 }
 
-// Note on this function. The foreground will be pushed 1 column right and / or down when the
-// background width or height are even.
+// Note on this function. The foreground will be pushed 1 column left and / or up when the
+// position is center, but the centering calculation does not have mod 0.
 func Test_offsets(t *testing.T) {
 	tests := []struct {
 		name      string
@@ -303,6 +303,302 @@ func Test_offsets(t *testing.T) {
 			x, y := offsets(test.fg, test.bg, test.xPos, test.yPos, test.xOff, test.yOff)
 			assert.Equal(t, test.expectedX, x, "x should be equal")
 			assert.Equal(t, test.expectedY, y, "y should be equal")
+		})
+	}
+}
+
+func Test_composite(t *testing.T) {
+	fg := strings.Repeat("abc\n", 2) + "abc"
+	bg := strings.Repeat("1234567\n", 6) + "1234567"
+
+	tests := []struct {
+		name     string
+		xPos     Position
+		yPos     Position
+		xOff     int
+		yOff     int
+		fg       string
+		bg       string
+		expected string
+	}{
+		{
+			"centered, no offset",
+			Center,
+			Center,
+			0,
+			0,
+			fg,
+			bg,
+			strings.Repeat(
+				"1234567\n",
+				2,
+			) + strings.Repeat(
+				"12abc67\n",
+				3,
+			) + "1234567\n1234567",
+		},
+		{
+			"centered, with offset",
+			Center,
+			Center,
+			1,
+			1,
+			fg,
+			bg,
+			strings.Repeat(
+				"1234567\n",
+				3,
+			) + strings.Repeat(
+				"123abc7\n",
+				3,
+			) + "1234567",
+		},
+		{
+			"top left, no offset",
+			Left,
+			Top,
+			0,
+			0,
+			fg,
+			bg,
+			strings.Repeat(
+				"abc4567\n",
+				3,
+			) + strings.Repeat(
+				"1234567\n",
+				3,
+			) + "1234567",
+		},
+		{
+			"top left, with offset",
+			Left,
+			Top,
+			1,
+			1,
+			fg,
+			bg,
+			"1234567\n" + strings.Repeat(
+				"1abc567\n",
+				3,
+			) + strings.Repeat(
+				"1234567\n",
+				2,
+			) + "1234567",
+		},
+		{
+			"top center, no offset",
+			Center,
+			Top,
+			0,
+			0,
+			fg,
+			bg,
+			strings.Repeat(
+				"12abc67\n",
+				3,
+			) + strings.Repeat(
+				"1234567\n",
+				3,
+			) + "1234567",
+		},
+		{
+			"top center, with offset",
+			Center,
+			Top,
+			1,
+			1,
+			fg,
+			bg,
+			"1234567\n" + strings.Repeat(
+				"123abc7\n",
+				3,
+			) + strings.Repeat(
+				"1234567\n",
+				2,
+			) + "1234567",
+		},
+		{
+			"top right, no offset",
+			Right,
+			Top,
+			0,
+			0,
+			fg,
+			bg,
+			strings.Repeat(
+				"1234abc\n",
+				3,
+			) + strings.Repeat(
+				"1234567\n",
+				3,
+			) + "1234567",
+		},
+		{
+			"top right, with offset",
+			Right,
+			Top,
+			-1,
+			1,
+			fg,
+			bg,
+			"1234567\n" + strings.Repeat(
+				"123abc7\n",
+				3,
+			) + strings.Repeat(
+				"1234567\n",
+				2,
+			) + "1234567",
+		},
+		{
+			"center left, no offset",
+			Left,
+			Center,
+			0,
+			0,
+			fg,
+			bg,
+			strings.Repeat(
+				"1234567\n",
+				2,
+			) + strings.Repeat(
+				"abc4567\n",
+				3,
+			) + "1234567\n1234567",
+		},
+		{
+			"center left, with offset",
+			Left,
+			Center,
+			1,
+			1,
+			fg,
+			bg,
+			strings.Repeat(
+				"1234567\n",
+				3,
+			) + strings.Repeat(
+				"1abc567\n",
+				3,
+			) + "1234567",
+		},
+		{
+			"center right, no offset",
+			Right,
+			Center,
+			0,
+			0,
+			fg,
+			bg,
+			strings.Repeat(
+				"1234567\n",
+				2,
+			) + strings.Repeat(
+				"1234abc\n",
+				3,
+			) + "1234567\n1234567",
+		},
+		{
+			"center right, with offset",
+			Right,
+			Center,
+			-1,
+			1,
+			fg,
+			bg,
+			strings.Repeat(
+				"1234567\n",
+				3,
+			) + strings.Repeat(
+				"123abc7\n",
+				3,
+			) + "1234567",
+		},
+		{
+			"bottom right, no offset",
+			Right,
+			Bottom,
+			0,
+			0,
+			fg,
+			bg,
+			strings.Repeat(
+				"1234567\n",
+				4,
+			) + strings.Repeat(
+				"1234abc\n",
+				2,
+			) + "1234abc",
+		},
+		{
+			"bottom right, with offset",
+			Right,
+			Bottom,
+			-1,
+			-1,
+			fg,
+			bg,
+			strings.Repeat(
+				"1234567\n",
+				3,
+			) + strings.Repeat(
+				"123abc7\n",
+				3,
+			) + "1234567",
+		},
+		{
+			"bottom left, no offset",
+			Left,
+			Bottom,
+			0,
+			0,
+			fg,
+			bg,
+			strings.Repeat(
+				"1234567\n",
+				4,
+			) + strings.Repeat(
+				"abc4567\n",
+				2,
+			) + "abc4567",
+		},
+		{
+			"bottom left, with offset",
+			Left,
+			Bottom,
+			1,
+			-1,
+			fg,
+			bg,
+			strings.Repeat(
+				"1234567\n",
+				3,
+			) + strings.Repeat(
+				"1abc567\n",
+				3,
+			) + "1234567",
+		},
+		{
+			"bottom left, out of bounds offset",
+			Left,
+			Bottom,
+			10,
+			10,
+			fg,
+			bg,
+			strings.Repeat(
+				"1234567\n",
+				4,
+			) + strings.Repeat(
+				"1234abc\n",
+				2,
+			) + "1234abc",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			x := composite(test.fg, test.bg, test.xPos, test.yPos, test.xOff, test.yOff)
+			assert.Equal(t, test.expected, x, "x should be equal")
 		})
 	}
 }
